28b1967adf41e058c8fef0e618b42456f4de0dbc
JEONG <prophet000228@gmail.com>
2025-05-09 21:33:02 +0900

    feat : kakaoMap Load

--- front_end/src/component/DisplayPage.js
+++ front_end/src/component/DisplayPage.js
@@ -1,9 +1,13 @@
 // CCTVMap.jsx
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { useKakaoLoader } from "react-kakao-maps-sdk";
 import { Map, MapMarker } from "react-kakao-maps-sdk";
 
 function CCTVMap() {
+  const [loading, error] = useKakaoLoader({
+    appkey: process.env.REACT_APP_KAKAOMAP_KEY,
+  });
   const [cctvs, setCctvs] = useState([]);
 
   useEffect(() => {
@@ -17,6 +21,21 @@ function CCTVMap() {
         console.error("CCTV 불러오기 실패:", err);
       });
   }, []);
+
+  console.log(loading, error)
+
+  return ( 
+    <>
+        <Map
+            // 카카오맵 API 키를 사용하여 맵을 초기화합니다.
+            center={{ lat: 35.871183, lng: 128.601599 }}
+            style={{ width: "390px", height: "844px" }}
+            level={3}
+        >
+        {/* CCTV 마커들 표시 */}
+</Map>
+    </>
+  )
 
   
 }
